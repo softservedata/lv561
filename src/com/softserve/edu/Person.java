12273c721190d717ee18177b42ec10f8399a5bb5
YuriiVern <yuriy.vernygorov@gmail.com>
2021-01-27 01:12:46 +0200

    class Student done

--- src/com/softserve/edu/Person.java
+++ src/com/softserve/edu/Person.java
@@ -1,2 +1,36 @@
-package com.softserve.edu;public class Person {
+package com.softserve.edu;
+
+public class Person {
+                            //field initialization //
+    private String firstName;
+    private String secondName;
+    private int birthYear;
+                            // default contructor
+    public Person () {
+
+    }
+                            // constructor with parameters
+    public Person (String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+                            //getAge method as years diff
+    public int getAge() {
+        int currentYear = 2021;
+        return currentYear - this.birthYear;
+    }
+
+    public void changeName(String fn, String sn) {
+        this.firstName = fn;
+        this.secondName = sn;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Person "+ firstName+" "+secondName+" is ";
+    }
 }
