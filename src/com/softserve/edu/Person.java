a42ff5522cadf9ecfaa0db8c0dc81eb9350ceb07
YuriiVern <yuriy.vernygorov@gmail.com>
2021-01-27 13:09:42 +0200

    class Person done with some notes. No information about input() and output  methods

--- src/com/softserve/edu/Person.java
+++ src/com/softserve/edu/Person.java
@@ -1,11 +1,12 @@
 package com.softserve.edu;
 
+
 public class Person {
                             //field initialization //
     private String firstName;
     private String secondName;
     private int birthYear;
-                            // default contructor
+                            // default constructor
     public Person () {
 
     }
@@ -25,12 +26,37 @@ public class Person {
         this.secondName = sn;
     }
 
+    public void input() {
+
+    }
+
+
+    public void output() {
+
+        System.out.println(this.toString());
+    }
+
     public static void main(String[] args) {
+        Person person1 = new Person("Ivan", "Romanyk");
+        person1.birthYear = 1988;
+        Person person2 = new Person("Petro","Poroshenko");
+        person2.birthYear = 1965;
+        Person person3 = new Person("Zhan", "Beleniyk");
+        person3.birthYear = 1985;
+        Person person4 = new Person("Leonid", " Kadeniyk");
+        person4.birthYear = 1925;
+        Person person5 = new Person("Andriy","Shevchenko");
+        person5.birthYear = 1977;
+
+        Person[] people = new Person[] {person1, person2, person3, person4, person5};
+        for (Person person : people) {
+            person.output();
+        }
 
     }
 
     @Override
     public String toString() {
-        return "Person "+ firstName+" "+secondName+" is ";
+        return "Person "+ firstName+" "+secondName+" war born in " +birthYear+" and is "+getAge()+" years old";
     }
 }
