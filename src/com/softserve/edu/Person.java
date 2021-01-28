a099f95ee37cf7db2c415b4bb377e6dcf21bedf1
YuriiVern <yuriy.vernygorov@gmail.com>
2021-01-28 14:36:13 +0200

    method input() is complete

--- src/com/softserve/edu/Person.java
+++ src/com/softserve/edu/Person.java
@@ -1,6 +1,13 @@
 package com.softserve.edu;
 
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Person {
                             //field initialization //
     private String firstName;
@@ -26,31 +33,60 @@ public class Person {
         this.secondName = sn;
     }
 
-    public void input() {
+    public static Person input() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = new Person();
 
+        try {
+            System.out.println("Enter firstname of your person instance: ");
+            String firstName = reader.readLine();
+                if (firstName.isEmpty() || firstName.equals("")) {
+                System.out.println("Entered empty firstname. Re-run app");
+                }
+            person.firstName = firstName;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+             System.out.println("Enter secondname of your person instance: ");
+             String secondName = reader.readLine();
+                if (secondName.isEmpty() || secondName.equals("")) {
+                    System.out.println("Entered empty secondname. Re-run app");
+                }
+                person.secondName = secondName;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        try {
+            System.out.println("Enter date of birth of your person instance: ");
+            int birthYear = Integer.parseInt(reader.readLine());
+                if (birthYear == 0 || birthYear <= 1900 || birthYear > 2021) {
+                System.out.println("Entered empty date of birth or wrong birth year. Re-run app");
+                }
+                person.birthYear = birthYear;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Person is created");
+        return person;
     }
 
 
-    public void output() {
+    public static void output(Person person) {
 
-        System.out.println(this.toString());
+        System.out.println(person.toString());
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("Ivan", "Romanyk");
-        person1.birthYear = 1988;
-        Person person2 = new Person("Petro","Poroshenko");
-        person2.birthYear = 1965;
-        Person person3 = new Person("Zhan", "Beleniyk");
-        person3.birthYear = 1985;
-        Person person4 = new Person("Leonid", " Kadeniyk");
-        person4.birthYear = 1925;
-        Person person5 = new Person("Andriy","Shevchenko");
-        person5.birthYear = 1977;
+        Person person1 = input();
+        Person person2 = input();
+        Person person3 = input();
+        Person person4 = input();
+        Person person5 = input();
 
         Person[] people = new Person[] {person1, person2, person3, person4, person5};
         for (Person person : people) {
-            person.output();
+            output(person);
         }
 
     }
