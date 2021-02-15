1247ce17b180bbd9e498d5f0c44a60d47efe2dae
Vitalii <vitkryshtop@gmail.com>
2021-02-15 17:23:50 +0200

    create uml in HW7

--- src/com/homework5/Car.java
+++ src/com/homework5/Car.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 /*Create class Car with fields type, year of production and engine capacity. Create and initialize four instances of class Car. Display cars
 certain model year  (enter year in the console);
 ordered by the field year.
-Створіть клас Car з типом полів, роком виробництва та потужністю двигуна.
-Створити та ініціалізувати чотири екземпляри класу Car. Вітрини автомобілів
-певний модельний рік (введіть рік у консолі);
-упорядкований по польовому року.
 */
 
 class Car {
@@ -63,14 +59,7 @@ class Car {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       /* Car c1 = new Car();
-        c1.setName("Toyota");
-        c1.setModel("Camry");
-        c1.setYear(2020);
-        c1.setCapasity(160);
-        Car c2 = new Car("Mersedes-Benz", "G63", 2017, 220);
-        Car c3 = new Car("Wolksvagen", "Passat", 2015, 150);
-        Car c4 = new Car("Opel", "Astra", 2017, 130);*/
+
 
         Car carFirst = new Car();
         carFirst.setType("Suzuki");
@@ -85,13 +74,7 @@ class Car {
         carFirst.setType("Tesla");
         carFirst.setEngineCapacity(0.0);
 
-      /*  System.out.println(c1.getName() + " " + c1.getModel() + " " + c1.getYear() + " " + c1.getCapasity());
 
-        System.out.println("Array of Cars: ");
-        Car[] cars = { c1, c2, c3, c4 };
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println(cars[i]);
-        }*/
 
     System.out.println(carFirst.getType() + " " + carFirst.getEngineCapacity() + " " + carFirst.getYearOfProduction());
         System.out.println("Array of Cars: ");
@@ -111,21 +94,7 @@ class Car {
         System.out.print(carFirst.getType() + " "
                 + carFirst.getEngineCapacity() + " " + carFirst.yearOfProduction);
 
-      /*  // Display cars certain model year (enter year in the console);
 
-        System.out.print("Input model year: ");
-        boolean isCars = false;
-        Scanner sc = new Scanner(System.in);
-        int year = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i < cars.length; i++) {
-            if (cars[i].getYear() == year) {
-                System.out.println(cars[i]);
-                isCars = true;
-            }
-        }
-        if (!isCars)
-            System.out.println("No cars to display!!!");
-        sc.close();*/
 
         System.out.println("Sorted by the field year");
         Car car = new Car();
@@ -147,40 +116,6 @@ class Car {
        }
 
 
-       /*// Create and initialize four instances of class Car.
 
 
 
-
-
-
-		// Ordered by the field year.
-		System.out.println("Ordered by the field year.");
-		Car tmp = new Car();
-		boolean isSorted = true;
-		for(int i = 0; i < cars.length-1;i++) {
-			isSorted = true;
-			//System.out.print("*");
-			for(int j = 0; j < cars.length-1-i;j++) {
-				if (cars[j].getYear() > cars[j+1].getYear()) {
-					tmp = cars[j];
-					cars[j]=cars[j+1];
-					cars[j+1] = tmp;
-					isSorted = false;
-					//System.out.print(".");
-				}
-
-			}
-			if(isSorted) {
-				break;
-
-			}
-
-		}
-		for (int i = 0; i < cars.length; i++) {
-			System.out.println(cars[i]);
-		}
-	}
-
-}
-© 2021 GitHub, Inc.*/
