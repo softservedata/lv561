6c6e6fed5f4def1e06e051dcfea95743a7cc1857
nadya <ndzioban@gmail.com>
2021-02-18 12:28:10 +0200

    Test commit

--- src/com/softserve/edu/Example.java
+++ src/com/softserve/edu/Example.java
@@ -3,7 +3,7 @@ package com.softserve.edu;
 public class Example {
 
 	public static void main(String[] args) {
-		System.out.println("My_first_program_!");
+		System.out.println("My__first_program_!");
 	}
 
 }
