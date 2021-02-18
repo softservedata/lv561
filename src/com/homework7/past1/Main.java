87271d3b17b7080e56ef8393b6d033cd2d2061c6
Vitalii <vitkryshtop@gmail.com>
2021-02-18 22:53:18 +0200

    other

--- src/com/homework7/past1/Main.java
+++ src/com/homework7/past1/Main.java
@@ -8,14 +8,7 @@ Output the entities of the map on the screen.
 There are at less two persons with the same firstName among these 10 people?
 Remove from the map person whose firstName is ”Orest” (or other). Print result.
 
-Записати параметризовані методи об'єднання (Set set1, Set set2) і перетинання (Set set1, Set set2),
-реалізація операцій об’єднання та перетину двох множин.
-Перевірте дію цих методів на двох попередньо заповнених наборах.
 
-Створіть карту personMap і додайте до неї десять осіб типу <String, String> (lastName, firstName).
-Виведіть об'єкти карти на екран.
-Серед цих 10 людей менше двох людей з однаковим Іменем?
-Видаліть з карти людину, чиє ім’я - “Орест” (або інше). Результат друку.
  */
 
 
---
+++ src/com/homework8/homework/Main.java
@@ -0,0 +1,21 @@
+package com.homework8.homework;
+
+/*Enter in the console sentence of five words.
+display the longest word in the sentence
+determine the number of its letters
+bring the second word in reverse order
+Enter a sentence that contains the words between more than one space.
+Convert all spaces, consecutive, one. For example,
+if we introduce the sentence "I    am      learning     Java   Core»,
+we have to get the "I'm learning Java Core»
+Implement a pattern for US currency: the first symbol "$",
+then any number of digits, dot and two digits after the dot.
+Enter the text from the console that contains several occurrences of US currency.
+Display all occurrences on the screen.
+* */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
