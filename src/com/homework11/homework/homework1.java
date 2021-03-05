c87c52992a6fc49578dc4044d291f5297a2d0d1e
Vitalii <vitkryshtop@gmail.com>
2021-03-05 23:23:57 +0200

    change HW11 without 2part

--- src/com/homework11/homework/homework1.java
+++ src/com/homework11/homework/homework1.java
@@ -6,18 +6,21 @@ package com.homework11.homework;
    * and length less than 5 characters (use method removeIf)
    */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 
 public class homework1 {
     public static void main(String[] args){
-        List<String> list1 = Arrays.asList("first", "second", "third", "fifth", "");
-        List<String> list2 = list1.stream()
-               // .filter(list -> list1.length() < 5)
-              //  .filter( )
-                .collect(Collectors.toList());
+        List<String> list1 = new ArrayList<>(Arrays.asList("Tom", "sim" , "Kiev", "Ljubljana", "Autumn"));;
+        System.out.println(list1);
+        List<String> list2 = new ArrayList<>();
+        list1.removeIf(x -> x.length()<3);
+        list2 = list1;
+        System.out.println(list2);
+
 
     }
 }
--- src/com/homework11/homework/homework2.java
+++ src/com/homework11/homework/homework2.java
@@ -1,7 +1,7 @@
 package com.homework11.homework;
 /*
  * 2. Create two methods String encrypt(String s, int n)
- * and String decrypt(Stirng s, int n)
+ * and String decrypt(String s, int n)
  * Method encrypt should take a string and return coded
  * string where every letter is moved on n positions in alphabet
  * (e.g. encrypt("abc", 3) returns "def“).
