3bac9863d19f2d39d7e63cb43022d13185f7f3d9
Anastasia Radchuk <71091432+ana-radchuk@users.noreply.github.com>
2021-03-09 13:28:52 +0200

    Update DateChecker.java

--- hw10_java8/DateChecker.java
+++ hw10_java8/DateChecker.java
@@ -7,9 +7,9 @@ package hw10_java8;
 public class DateChecker {
 
 	public static boolean matchesDatePattern(String dateString) {
-	    return dateString.matches("^((0[1-9]|[12][0-9]|30).(0[469]|11).\\d{2})$" 
-	    					   + "|^((0[1-9]|[12][0-9]|3[01]).(0[13578]|10|12).\\d{2})$" 
-	    					   + "|^((0[1-9]|[12][0-9]).02.\\d{2})$"); 
+	    return dateString.matches("^((0[469]|11).(0[1-9]|[12][0-9]|30).\\d{2})$" 
+	    					   + "|^((0[13578]|10|12).(0[1-9]|[12][0-9]|3[01]).\\d{2})$" 
+	    					   + "|^(02.(0[1-9]|[12][0-9]).\\d{2})$"); 
 	}
 	
 }
